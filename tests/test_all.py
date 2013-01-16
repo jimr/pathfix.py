af46b404c3736e6ef31100fb5bef63998d282cba
James Rutherford <jim@jimr.org>
2013-01-16 15:44:39 +0000

    Added tests for //host/share/path URLs

--- tests/test_all.py
+++ tests/test_all.py
@@ -15,10 +15,10 @@ class TestAll(unittest.TestCase):
         )
 
     def test_drive_maps(self):
-        fixed = pathfix.fix_path('X:\some\path', self.cfg)
+        fixed = pathfix.fix_path(r'X:\some\path', self.cfg)
         self.assertEqual(fixed, '/media/network/host1/share1/some/path')
 
-        fixed = pathfix.fix_path('Y:\some path\with%20spaces', self.cfg)
+        fixed = pathfix.fix_path(r'Y:\some path\with%20spaces', self.cfg)
         self.assertEqual(
             fixed,
             '/media/network/host2/share2/some path/with spaces'
@@ -42,12 +42,19 @@ class TestAll(unittest.TestCase):
         fixed = pathfix.fix_path('smb://host2/share2/some/path', self.cfg)
         self.assertEqual(fixed, '/media/network/host2/share2/some/path')
 
+    def test_slash_prefix(self):
+        fixed = pathfix.fix_path('//host1/share1/some/path', self.cfg)
+        self.assertEqual(fixed, '/media/network/host1/share1/some/path')
+
+        fixed = pathfix.fix_path(r'\\host2\share2\some\path', self.cfg)
+        self.assertEqual(fixed, '/media/network/host2/share2/some/path')
+
     def test_unicode_path(self):
         fixed = pathfix.fix_path('file://host1/share1/ßøŋ', self.cfg)
         self.assertEqual(fixed, '/media/network/host1/share1/ßøŋ')
 
     def test_quoted_chars(self):
-        fixed = pathfix.fix_path('X:\\\\is%23this%3Aa%20file%3F.txt', self.cfg)
+        fixed = pathfix.fix_path(r'X:\\is%23this%3Aa%20file%3F.txt', self.cfg)
         self.assertEqual(
             fixed,
             '/media/network/host1/share1/is#this:a file?.txt'
